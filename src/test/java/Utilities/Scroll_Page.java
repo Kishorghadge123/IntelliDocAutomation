3fb2d87e3d8922fd7586fea272de7f38dd087200
NTS-AmitKorade <amitark1995@gmail.com>
2022-10-04 13:05:40 +0530

    Scroll

--- src/test/java/Utilities/Scroll_Page.java
+++ src/test/java/Utilities/Scroll_Page.java
@@ -11,23 +11,7 @@ public class Scroll_Page extends BaseTest
 	public Scroll_Page(WebDriver driver) {
 		this.driver = driver;
 	}
-	public static void scroll_down_page(WebDriver driver) throws Exception
-	{
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,250)", "");
-	}
 
-	public static void scroll_down_page_till_end(WebDriver driver) throws Exception
-	{
-	     JavascriptExecutor js = (JavascriptExecutor) driver;
-	   js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
-	}
-
-	public static void scroll_up_page(WebDriver driver) throws Exception
-	{
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,-350)", "");
-	}
 	public void ScrollUpDown(WebElement element) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].scrollIntoView();", element);
